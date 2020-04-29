b7aebd1bc6f7ffbcb2389898b028743120102b73
Nesnes9 <57038390+Nesnes9@users.noreply.github.com>
2020-04-29 11:54:12 +0200

    Update Exo1.rb

--- Exo1.rb
+++ Exo1.rb
@@ -4,8 +4,8 @@ chaine1 = gets.chomp!
 puts "Saisir la deuxième chaine de caractère : "
 chaine2 = gets.chomp!
 
-if chaine1.length > chaine2.length
-  puts "La chaine 1 est plus grande que la chaine 2"
+if (chaine1.length)*2 > chaine2.length
+  puts "La chaine1 est deux fois plus grande que la chaine2"
 else
-  puts "La chaine 2 est plus grande que la chaine 1"
+  puts "La chaine1 n'est pas deux fois plus grande que la chaine2"
 end
